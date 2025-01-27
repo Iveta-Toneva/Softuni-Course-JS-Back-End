1821fa9ee2e927de3c312e0c832cffaf1b9e8d04
Iveta-Toneva <iveta.v.toneva@gmail.com>
2025-01-27 12:23:43 +0000

    .add register page and functionality

--- movie-magic-workshop /routes.js
+++ movie-magic-workshop /routes.js
@@ -2,12 +2,14 @@ import { Router } from 'express';
 import homecontroller from './src/controllers/homeController.js';
 import movieController from './src/controllers/movieController.js';
 import castController from './src/controllers/castController.js';
+import authController from './src/controllers/authController.js'
 
 const router = Router();
 
 router.use(homecontroller);
 router.use('/movies', movieController);
 router.use('/cast', castController);
+router.use('/auth', authController);
 router.all('*', (req, res) => {
     res.render('404');
 });
