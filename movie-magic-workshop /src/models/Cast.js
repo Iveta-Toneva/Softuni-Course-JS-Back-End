2817115283ae0d1cbe3cca8553c4bd3ce7717726
Iveta-Toneva <iveta.v.toneva@gmail.com>
2025-02-05 11:21:10 +0000

    .add cast validations

--- movie-magic-workshop /src/models/Cast.js
+++ movie-magic-workshop /src/models/Cast.js
@@ -1,10 +1,29 @@
 import { Schema, model } from 'mongoose';
 
 const castSchema = new Schema({
-    name: String,
-    age: Number,
-    born: String,
-    imageUrl: String
+    name: {
+        type: String,
+        required: [true, 'Name is required!'],
+        minLength: [5, 'Name should be at least 5 characters!'],
+        match: [/^[A-Za-z0-9 ]$/, 'The name can contain only english letters digits and whitespaces!']
+    },
+    age: {
+        type: Number,
+        requred: [true, 'Age is required!'],
+        min: [1, 'The age can not be less than 1!'],
+        max: [120, 'The age can not be more than 120!']
+    },
+    born: {
+        type: String,
+        required: [true, 'Born is required!'],
+        minLength: [10, 'Born should be at least 10 characters!'],
+        match: [/^[A-Za-z0-9 ]$/, 'The born can contain only english letters digits and whitespaces!']
+    },
+    imageUrl: {
+        type: String,
+        required: [true, 'Image is required!'],
+        match: [/^http:\/\/s?/, 'The image URL should be valid!']
+    }
 });
 
 
