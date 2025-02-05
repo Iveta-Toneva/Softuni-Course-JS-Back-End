00428e9ba0347bdcc851349d6d7bd59d49164b09
Iveta-Toneva <iveta.v.toneva@gmail.com>
2025-02-05 11:03:07 +0000

    add user validations

--- movie-magic-workshop /src/models/User.js
+++ movie-magic-workshop /src/models/User.js
@@ -4,8 +4,19 @@ import bcrypt from 'bcrypt';
 const SALT_ROUNDS = 10;
 
 const userSchema = new Schema({
-    email: String,
-    password: String
+    email: {
+        type: String,
+        unique: true,
+        required: [true, 'Email is required'],
+        minlength: [10, 'The email should be at least 10 characters long!'],
+        match: [/\@[A-Za-z0-9]+.[A-Za-z0-9]+$/, 'The email should be valid!'],
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        match: [/^[A-Za-z0-9]+$/, 'The password can contain only english letters and digits'],
+        minlength: [6, 'The password should be at least 6 characters long !']
+    }
 });
 
 userSchema.pre('save', async function () {
