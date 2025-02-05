80f3ba4355292e04763961c6c13ac8c91f0b9159
Iveta-Toneva <iveta.v.toneva@gmail.com>
2025-02-05 11:16:06 +0000

    .add movie validations

--- movie-magic-workshop /src/models/Movie.js
+++ movie-magic-workshop /src/models/Movie.js
@@ -3,14 +3,47 @@ import { Schema, model, Types } from 'mongoose';
 const movieSchema = new Schema({
     title: {
         type: String,
-        required: true
+        required: [true, 'Title is required!'],
+        minLength: [5, 'Title should be at least 5 characters!'],
+        match: [/^[A-Za-z0-9 ]$/, 'Title can contain only english letters digits and whitespaces!']
     },
-    genre: String,
-    director: String,
-    year: Number,
-    imageUrl: String,
-    rating: Number,
-    description: String,
+    genre: {
+        type: String,
+        required: [true, 'Genre is required!'],
+        minLength: [5, 'Genre should be at least 5 characters!'],
+        match: [/^[A-Za-z0-9 ]$/, 'Genre can contain only english letters digits and whitespaces!']
+    },
+    director: {
+        type: String,
+        required: [true, 'Director is required!'],
+        minLength: [5, 'Director should be at least 5 characters!'],
+        match: [/^[A-Za-z0-9 ]$/, 'Director can contain only english letters digits and whitespaces!']
+    },
+    year: {
+        type: Number,
+        required: [true, 'Year is required!'],
+        min: [1900, 'The year should be after 1900!'],
+        max: [2024, 'The year can not be after 2024']
+    },
+    imageUrl: {
+        type: String,
+        required: [true, 'Image is required!'],
+        match: [/^http:\/\/s?/, 'The image URL should be valid!']
+
+    },
+    rating: {
+        type: Number,
+        min: [1, 'The rating should be at least 1'],
+        max: [5, 'The rating can not be more than 5'],
+        required: [true, 'Rating is required!']
+    },
+    description: {
+        type: String,
+        required: [true, 'Description is required!'],
+        minLength: 20,
+        match: [/^[A-Za-z0-9 ]$/, 'Description can contain only english letters digits and whitespaces!']
+
+    },
     casts: [{
         type: Types.ObjectId,
         ref: 'Cast'
