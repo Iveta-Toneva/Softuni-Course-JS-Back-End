cf69af7fa36942918d1e68c6df2324dfb39fc227
Iveta-Toneva <iveta.v.toneva@gmail.com>
2025-02-22 14:35:27 +0000

    Update User.js

--- movie-magic-workshop /src/models/User.js
+++ movie-magic-workshop /src/models/User.js
@@ -6,13 +6,13 @@ const userSchema = new Schema({
     email: {
         type: String,
         unique: true,
-        required: [true, 'Email is required'],
+        required: [true, 'Email is required!'],
         minlength: [10, 'The email should be at least 10 characters long!'],
         match: [/\@[A-Za-z0-9]+.[A-Za-z0-9]+$/, 'The email should be valid!'],
     },
     password: {
         type: String,
-        required: [true, 'Password is required'],
+        required: [true, 'Password is required!'],
         match: [/^[A-Za-z0-9]+$/, 'The password can contain only english letters and digits'],
         minlength: [6, 'The password should be at least 6 characters long !']
     }
